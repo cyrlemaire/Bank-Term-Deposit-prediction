5c85aa6dbd67d557ae6f60878266908a2a2bf685
cyrlemaire <72152608+cyrlemaire@users.noreply.github.com>
2021-03-03 09:03:38 +0100

    Add preprocessing function

--- subscription_forecast/infrastructure/preprocessing.py
+++ subscription_forecast/infrastructure/preprocessing.py
@@ -5,6 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 
 # some parameters to put in config
@@ -14,33 +15,65 @@ socio_eco_features = ['employment_variation_rate', 'idx_consumer_price', 'idx_co
 numeric_features = ['balance', 'age', 'nb_day_last_contact', 'nb_contact']
 target = ['subscription']
 
+# config variables:
 
-def load_data_from(data_path: str, filename: str) -> pd.DataFrame:
-    """ load a dataset from in a .csv file and return a dataframe
-    with lower case column names"""
-    data = pd.read_csv(data_path+'/'+filename, delimiter=';')
-    data.columns = data.columns.str.lower()
-    return data
+data_path = '/Users/cyrillemaire/Documents/Yotta/Project/data/train'
+client_data_file_name = 'data.csv'
+socio_eco_file_name = 'socio_eco.csv'
 
-
-def socio_eco_imputer(socio_eco):
-    """Fills the NaN values specifically for the socio_eco dataset."""
-    socio_eco_complete = socio_eco.copy()
-    socio_eco_complete.iloc[[4, 8, 9, 20, 21], 1] = [-0.1, -0.2, -0.2, -3.0, 3.0]
-    socio_eco_complete = socio_eco_complete.interpolate()
-    return socio_eco_complete
+output_file_path = '/Users/cyrillemaire/Documents/Yotta/Project/EDA'
+output_file_name = 'data_profiling_EDA.html'
 
 
-def link_dataframes(data_left, data_right):
-    """Extract month and year from a date column in 2 dataframes,
-    use this truncated date as key for a left join between the two datasets"""
-    data_left['date_trunc'] = data_left['date'].str.slice(stop=7)
-    data_right['date_trunc'] = data_right['date'].str.slice(stop=7)
-    data_left = (pd.merge(data_left, data_right, left_on='date_trunc', right_on='date_trunc', how='left'))
-    data_left.drop('date_trunc', axis=1, inplace=True)
-    data_left.drop('date_y', axis=1, inplace=True)
-    data_left.rename(columns={'date_x': 'date'}, inplace=True)
-    return data_left
+def train_test_from(data_path: str, client_data_file_name: str, socio_eco_file_name: str):
+    """extract the 2 csv files and create a dataset to feed in the
+    Feature engineering pipeline"""
+
+    def load_data_from(data_path: str, filename: str) -> pd.DataFrame:
+        """ load a dataset from in a .csv file and return a dataframe
+        with lower case column names"""
+        data = pd.read_csv(data_path+'/'+filename, delimiter=';')
+        data.columns = data.columns.str.lower()
+        return data
+
+
+    def socio_eco_imputer(socio_eco):
+        """Fills the NaN values specifically for the socio_eco dataset."""
+        socio_eco_complete = socio_eco.copy()
+        socio_eco_complete.iloc[[4, 8, 9, 20, 21], 1] = [-0.1, -0.2, -0.2, -3.0, 3.0]
+        socio_eco_complete = socio_eco_complete.interpolate()
+        return socio_eco_complete
+
+
+    def link_dataframes(data_left, data_right):
+        """Extract month and year from a date column in 2 dataframes,
+        use this truncated date as key for a left join between the two datasets"""
+        data_left['date_trunc'] = data_left['date'].str.slice(stop=7)
+        data_right['date_trunc'] = data_right['date'].str.slice(stop=7)
+        data_left = (pd.merge(data_left, data_right, left_on='date_trunc', right_on='date_trunc', how='left'))
+        data_left.drop('date_trunc', axis=1, inplace=True)
+        data_left.drop('date_y', axis=1, inplace=True)
+        data_left.rename(columns={'date_x': 'date'}, inplace=True)
+        return data_left
+
+    def set_split(full_data):
+        y = full_data[target]
+        X = full_data.drop[target]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return X_train, X_test, y_train, y_test
+
+    # load data
+    client = load_data_from(data_path, client_data_file_name)
+    socio_eco = load_data_from(data_path, socio_eco_file_name)
+    socio_eco = socio_eco_imputer(socio_eco)
+
+    # merge data
+    client_full = link_dataframes(client, socio_eco)
+
+    #set_split:
+    X_train, X_test, y_train, y_test = set_split(client_full)
+
+    return X_train, X_test, y_train, y_test
 
 
 class IndicatorTransformer(BaseEstimator, TransformerMixin):
