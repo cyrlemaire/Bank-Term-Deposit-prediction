a457ecf48bf4e954061954cd5c2f2d99c149ac87
cyrlemaire <72152608+cyrlemaire@users.noreply.github.com>
2021-04-29 10:03:39 +0200

    refactor predict with name == main

--- subscription_forecast/application/final_model_prediction.py
+++ subscription_forecast/application/final_model_prediction.py
@@ -14,34 +14,40 @@ MODEL_NAME = CONFIG['model']['name']
 
 features_to_drop = CONFIG['filters']['features_to_drop']
 
-# get data for prediction:
-
-prediction_data_full = preprocessing.features_from(CONFIG['prediction_data']['data_path'],
-                                                   CONFIG['prediction_data']['client_file_name'],
-                                                   CONFIG['prediction_data']['socio_eco_file_name'],
-                                                   features_to_drop)
-
 # Load model from pickle:
 
 def load_model():
     with open("/Users/cyrillemaire/Documents/Yotta/Project/FINAL_TEST/productsubscription_dc_cl_js/subscription_forecast/finalized_model.sav", 'rb') as f:
         return pickle.load(f)
 
-
 final_pipeline = load_model()
 
-# get prediction probabilities:
 
-engineered_data = final_pipeline.steps[0][1].transform(prediction_data_full)
-predictions_proba = final_pipeline.steps[1][1].predict_proba(engineered_data)
+def main():
 
-# get predictions with given threshold:
+    # get data for prediction:
 
-THRESHOLD = CONFIG['model']['threshold']
-predictions = np.where(predictions_proba[:, 1] >= THRESHOLD, 1, 0)
+    prediction_data_full = preprocessing.features_from(CONFIG['prediction_data']['data_path'],
+                                                       CONFIG['prediction_data']['client_file_name'],
+                                                       CONFIG['prediction_data']['socio_eco_file_name'],
+                                                       features_to_drop)
 
-# create and store a dataframe with client indexes and predictions :
+    # get prediction probabilities:
 
-predictions_output = pd.DataFrame(data=predictions, index=prediction_data_full.index, columns=['predictions'])
-predictions_output.to_csv(CONFIG['prediction_data']['data_path']+'/'+CONFIG['prediction_data']['predictions_file_name'],
-                          index=True)
+    engineered_data = final_pipeline.steps[0][1].transform(prediction_data_full)
+    predictions_proba = final_pipeline.steps[1][1].predict_proba(engineered_data)
+
+    # get predictions with given threshold:
+
+    THRESHOLD = CONFIG['model']['threshold']
+    predictions = np.where(predictions_proba[:, 1] >= THRESHOLD, 1, 0)
+
+    # create and store a dataframe with client indexes and predictions :
+
+    predictions_output = pd.DataFrame(data=predictions, index=prediction_data_full.index, columns=['predictions'])
+    predictions_output.to_csv(CONFIG['prediction_data']['data_path']+'/'+CONFIG['prediction_data']['predictions_file_name'],
+                              index=True)
+    return predictions_output
+
+if __name__ == '__main__':
+    main()
